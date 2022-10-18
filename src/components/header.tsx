f14f3cc6527b45623f42c3713f8a46e8247e89b0
2k22-Leap2-Tengis <tengis@iMac-20s-iMac.local>
2022-10-18 11:43:19 +0800

    dad

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import CSS  from 'csstype'
+const Header = () => {
+    return(
+        <div style={headerStyle}>
+
+        </div>
+    )
+}
+export default Header;
+const headerStyle: CSS.Properties = {
+    width:'100vw',
+    height:'10vh',
+    backgroundColor:'red'
+}
---
+++ src/pages/landing.tsx
@@ -0,0 +1,10 @@
+import Header from "../components/header";
+
+const Landing = () => {
+    return(
+        <div>
+            <Header/>
+        </div>
+    )
+}
+export default Landing;
